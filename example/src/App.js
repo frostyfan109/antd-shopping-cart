6d3f51c4593ee99ee6a18160cbe01a4317feff58
Griffin Roupe <groupe@renci.org>
2022-07-26 13:41:25 -0400

    Creates boilerplate example.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,68 @@
-import React from 'react'
+import { Layout, Menu, Typography } from 'antd'
+import { ShoppingOutlined } from '@ant-design/icons'
+import { Link, LocationProvider, Router, useLocation } from '@reach/router'
+import { ShoppingCartProvider } from 'antd-shopping-cart'
+import { ProductsPage } from './products-page'
+import { CartPage } from './cart-page'
+import 'antd/dist/antd.css'
+import 'antd-shopping-cart/dist/bundle.css'
 
-import { ExampleComponent } from 'antd-shopping-cart'
-import 'antd-shopping-cart/dist/index.css'
+const { Title, Text } = Typography
+const { Header, Content, Footer } = Layout
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const location = useLocation()
+
+  return (
+    <ShoppingCartProvider
+      buckets={[{
+        id: "items",
+        name: "Items",
+        itemName: "item"
+      }]}
+      localStorageKey="example_carts"
+      defaultCartName="My cart"
+    >
+      <Layout className="layout" style={{ minHeight: "100%" }}>
+        <Header style={{ display: "flex", alignItems: "center", background: "#fff" }}>
+          <Link to="/">
+            <Title
+              level={ 4 }
+              style={{
+                float: "left",
+                color: "#1890ff",
+                marginBottom: 0,
+                marginRight: 8,
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+              <ShoppingOutlined style={{ fontSize: 36, marginRight: 8 }} />
+              E-Commerce Site
+            </Title>
+          </Link>
+          <Menu theme="light" mode="horizontal" selectedKeys={ location.pathname } style={{ flex: 1 }}>
+            <Menu.Item key="/">
+              <Link to="/">Products</Link>
+            </Menu.Item>
+            <Menu.Item key="/cart">
+              <Link to="/cart">Cart</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ background: "#f0f2f5", padding: 32 }}>
+          <Router basepath="/">
+            <ProductsPage path="/" />
+            <CartPage path="/cart" />
+          </Router>
+        </Content>
+      </Layout>
+    </ShoppingCartProvider>
+  )
 }
 
-export default App
+export default () => (
+  <LocationProvider>
+    <App />
+  </LocationProvider>
+)
---
+++ example/src/products-page.js
@@ -0,0 +1,59 @@
+import { List, Card, Typography, Button } from 'antd'
+import { AddToCartDropdownButton } from 'antd-shopping-cart'
+
+const { Title, Text } = Typography
+
+const products = [
+    {
+        name: "Parrot",
+        description: "Parrots are intelligent birds. They have relatively large brains, they can learn, and they can use simple tools.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Red-browed_Amazon_parrot.jpg/1600px-Red-browed_Amazon_parrot.jpg?20071010185212",
+        price: 250
+    },
+    {
+        name: "Gerbil",
+        description: "A gentle and hardy animal, the Mongolian gerbil has become a popular small house pet.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Meriones_unguiculatus_%28wild%29.jpg",
+        price: 30
+    },
+]
+
+export const ProductsPage = () => {
+    return (
+        <List
+            grid={{ gutter: 16, column: 4 }}
+            dataSource={ products }
+            renderItem={ (item, i) => (
+                <List.Item key={ i }>
+                    <Card
+                        hoverable
+                        cover={ <img src={ item.image } /> }
+                        style={{ textAlign: "center" }}
+                        actions={[
+                            <div style={{ padding: "0 16px" }}>
+                                <AddToCartDropdownButton
+                                    item={{
+                                        id: item.name,
+                                        name: item.name,
+                                        description: item.description,
+                                        price: item.price,
+                                        tax: item.price * .075,
+                                        from: null,
+                                        bucketId: "items",
+                                        item: item,
+                                    }}
+                                    buttonProps={{ style: { width: "100%", minWidth: undefined } }}
+                                />
+                            </div>
+                        ]}
+                    >
+                        <Title level={ 5 } style={{ fontWeight: 500, marginBottom: 16 }}>{ item.name }</Title>
+                        <Text style={{ display: "block", fontSize: 15, marginBottom: 16 }}>${ item.price }</Text>
+                        <Text type="secondary">{ item.description }</Text>
+                        {/* <Card.Meta title={ item.name } description={ item.description }  /> */}
+                    </Card>
+                </List.Item>
+            ) }
+        />
+    )
+}
